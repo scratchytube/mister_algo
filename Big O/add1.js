dac5039b1e944ebf14ad036dcf7d546eb9f3c3f4
Brian Sahota <bpsahota@gmail.com>
2021-03-20 18:30:42 -0400

    coming together

---
+++ Big O/add1.js
@@ -0,0 +1,7 @@
+const addUpTo = (n) => {
+    total = 0
+    for( let i =1; i <= n, i++; ) {
+        total += i
+    }
+    console.log(addUpTo(6))
+} 
--- README.md
+++
@@ -1,2 +0,0 @@
-This will be the greatest algo course ever taken.
-We need the greatest algo warrior
